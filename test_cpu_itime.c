e91bdce483b709402a8dc1812177b4bfd14ea848
Farid Kurbanov <71608691+farid03@users.noreply.github.com>
2022-11-25 15:49:21 +0300

    Update test_cpu_itime.c

--- test_cpu_itime.c
+++ test_cpu_itime.c
@@ -3,21 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
+
+struct cpu_itimer {
+    uint64_t expires;
+    uint64_t incr;
+};
 
 int main(int argc, char** argv) {
 
     printf("----cpu_itimer syscall testing----\n");
-    struct cpu_itimer itimer = { 0 };
+    struct cpu_itimer itimer;
     pid_t pid = atoi(argv[1]);
     unsigned int itimer_type = atoi(argv[2]); // 0 or 1
     
-    long int ret_code = syscall(549, &dev, pid, itimer_type);
+    long int ret_code = syscall(549, &itimer, pid, itimer_type);
     printf("syscall ret code: %li\n", ret_code);
     printf("struct cpu_itimer {\n");
-    printf("  expires: %d\n", itimer.vendor);
-    printf("  incr: %d\n", itimer.device);
+    printf("  expires: %ld\n", itimer.expires);
+    printf("  incr: %ld\n", itimer.incr);
     printf("}\n");
     
     return 0;
 }
-
